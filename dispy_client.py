5b4ec6bf47faf9b604f7d31c8784a71833350f2b
x010 <jeffrey@JeffreydeMacBook-Pro.local>
2016-04-28 22:14:32 +0800

    init

--- dispy_client.py
+++ dispy_client.py
@@ -2,12 +2,15 @@
 # @Author: jeffrey
 # @Date:   2016-04-28T19:03:44+08:00
 # @Last modified by:   jeffrey
-# @Last modified time: 2016-04-28T22:11:36+08:00
+# @Last modified time: 2016-04-28T22:13:54+08:00
 
+#!/usr/bin/env python
+import dispy
+cluster = dispy.JobCluster('cat unittest/pcweb_pv.log|python luncher.py pcweb_pv 201604281859',nodes=['172.31.23.211','172.31.21.135'])
 
 if __name__ =='__main__':
     import dispy
     import os
     import sys
-    cluster=dispy.JobCluster('cat unittest/pcweb_pv.log|python luncher.py pcweb_pv 201604281859',nodes=['172.31.23.211','172.31.21.135'])    
+    cluster=dispy.JobCluster()
     cluster.print_status()
