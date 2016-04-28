95a20aa5ba43c31aa5e1e1104da91795ab35aba8
x010 <jeffrey@JeffreydeMacBook-Pro.local>
2016-04-28 22:11:53 +0800

    init

--- dispy_client.py
+++ dispy_client.py
@@ -2,21 +2,12 @@
 # @Author: jeffrey
 # @Date:   2016-04-28T19:03:44+08:00
 # @Last modified by:   jeffrey
-# @Last modified time: 2016-04-28T20:58:08+08:00
+# @Last modified time: 2016-04-28T22:11:36+08:00
 
 
 if __name__ =='__main__':
     import dispy
     import os
     import sys
-    cluster=dispy.JobCluster()
-    jobs=[]
-    for i in range(10):
-        job=cluster.submit('cat unittest/pcweb_pv.log|python luncher.py pcweb_pv 201604281859',nodes=['172.31.23.211','172.31.21.135'])
-        job.id=i
-        jobs.append(job)
-
-    for job in jobs:
-        host,n=job()
-        print('%s executed job %s at %s with %s' % (host, job.id, job.start_time, n))
+    cluster=dispy.JobCluster('cat unittest/pcweb_pv.log|python luncher.py pcweb_pv 201604281859',nodes=['172.31.23.211','172.31.21.135'])    
     cluster.print_status()
